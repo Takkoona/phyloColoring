a202fecf7b212672b6beeee1a74172cbaa4a0d02
Chengyang Ji <chengyang.ji12@alumni.xjtlu.edu.cn>
2022-09-22 08:18:56 +0800

    Update demo_figures.R

--- demo_figures.R
+++ demo_figures.R
@@ -11,10 +11,10 @@ if (dir.exists(output_plot_dir)) {
 dir.create(output_plot_dir, showWarnings = FALSE)
 
 plot_path_comparison <- function(paths,
-                                select1,
-                                select2,
-                                path_size = NULL,
-                                branch_size = 1) {
+                                 select1,
+                                 select2,
+                                 path_size = NULL,
+                                 branch_size = 1) {
     if (is.null(path_size)) {
         path_size <- 6
     }
@@ -44,10 +44,10 @@ plot_path_comparison <- function(paths,
 }
 
 plot_sub_paths <- function(paths,
-                         select = NULL,
-                         path_color = "blue",
-                         path_size = 2,
-                         branch_size = 1) {
+                           select = NULL,
+                           path_color = "blue",
+                           path_size = 2,
+                           branch_size = 1) {
     tree <- ape::as.phylo(paths)
     if (is.null(select)) {
         path_nodes <- unique(unlist(paths))
@@ -68,82 +68,44 @@ plot_sub_paths <- function(paths,
 
 # =======================================================================
 
-data(h3n2_align)
-data(h3n2_tree)
+data(zikv_align)
+data(zikv_tree)
 
-paths <- addMSA(h3n2_tree, alignment = h3n2_align)
-tree <- as.phylo(paths)
+zikv_paths <- addMSA(zikv_tree, alignment = zikv_align)
+tree <- as.phylo(zikv_paths)
 n_nodes <- ape::Nnode(tree, internal.only = FALSE)
 
-paths <- lineagePath(paths, 0.04)
-paths
-
-min_entropy <- sitesMinEntropy(paths)
-mutations <- fixationSites(min_entropy)
-para_sites <- parallelSites(min_entropy, minSNP = 10, mutMode = "exact")
-
-# =======================================================================
-
-snp <- SNPsites(paths)
-p <- plotMutSites(snp)
-p
-
-ggsave(
-    filename = file.path(output_plot_dir, "sparseMutations.svg"),
-    plot = p,
-    device = "svg",
-    width = 4.5,
-    height = 4.5
-)
-
-p <- plot_sub_paths(paths, path_color = "blue", branch_size = 0.2)
-p
-
-# =======================================================================
-
-ggsave(
-    filename = file.path(output_plot_dir, "lineages.svg"),
-    plot = p,
-    device = "svg",
-    width = 3,
-    height = 4.5
-)
-
-path_select <- list("0" = NULL, "1" = 1, "2" = c(1, 2), "3" = 2, "4" = 5)
-
-for (n in names(path_select)) {
-    p <- plot_sub_paths(
-        paths,
-        select = path_select[[n]],
-        path_color = "blue",
-        path_size = 1,
-        branch_size = 0.05
-    )
-    print(p)
-
-    ggsave(
-        filename = file.path(output_plot_dir, paste0("pathway", n, ".svg")),
-        plot = p,
-        device = "svg",
-        width = 1,
-        height = 1.3
-    )
-}
-
-# =======================================================================
-
 pdf(file = file.path(output_plot_dir, "sneakPeek.pdf"))
-sp <- sneakPeek(paths)
+sp <- sneakPeek(zikv_paths)
 invisible(dev.off())
 
-p <- plotSingleSite(para_sites, 192, showPath = TRUE) +
+zikv_paths <- lineagePath(zikv_paths, 16)
+zikv_min_entropy <- sitesMinEntropy(zikv_paths)
+zikv_para_sites <- parallelSites(zikv_min_entropy, minSNP = 1)
+zikv_fixed_sites <- fixationSites(zikv_min_entropy)
+
+intersect(allSitesName(zikv_fixed_sites), allSitesName(zikv_para_sites))
+
+# =======================================================================
+
+p_entropy <- plotSingleSite(zikv_min_entropy, site = 139)
+p_paths_mutations <- plotSingleSite(zikv_fixed_sites, site = 139)
+
+ggsave(
+    filename = file.path(output_plot_dir, "ZIKV_example.svg"),
+    plot = gridExtra::arrangeGrob(p_entropy, p_paths_mutations, ncol = 2),
+    device = "svg",
+    width = 10,
+    height = 6
+)
+
+p_mutations <- plotSingleSite(zikv_para_sites, site = 1118) +
     ggtitle(label = NULL, subtitle = NULL) +
     theme(legend.position = "none")
-p
 
 ggsave(
     filename = file.path(output_plot_dir, "mutations.svg"),
-    plot = p,
+    plot = p_mutations,
     device = "svg",
     width = 3.5,
     height = 3.5 * tree_height_ratio
@@ -151,12 +113,12 @@ ggsave(
 
 # =======================================================================
 
-select1 <- c(2, 4)
+select1 <- 2
 
 p <- plot_path_comparison(
-    paths,
+    zikv_paths,
     select1,
-    seq_along(paths)[-select1],
+    seq_along(zikv_paths)[-select1],
     path_size = 1.5,
     branch_size = 0.5
 )
@@ -172,10 +134,9 @@ ggsave(
 
 n <- 0
 
-for (select2 in seq_along(paths)[-select1]) {
+for (select2 in seq_along(zikv_paths)[-select1]) {
     n <- n + 1
-    p <- plot_path_comparison(paths, select1, select2, 1, 0.05)
-    print(p)
+    p <- plot_path_comparison(zikv_paths, select1, select2, 1, 0.05)
     ggsave(
         filename = file.path(
             output_plot_dir,
@@ -187,36 +148,3 @@ for (select2 in seq_along(paths)[-select1]) {
         height = 1 * tree_height_ratio
     )
 }
-
-# =======================================================================
-
-all_path_nodes <- table(unlist(paths))
-most_recent_nodes <- as.integer(names(which(all_path_nodes == 1)))
-
-path_nodes <- unique(unlist(paths))
-
-group <- rep(1, times = n_nodes)
-group[path_nodes] <- 0
-group <- factor(group)
-
-group_colors <- rep(2, times = n_nodes)
-group_colors[path_nodes] <- 1
-group_colors[most_recent_nodes] <- 0
-group_colors <- factor(group_colors)
-
-size <- rep(1, times = n_nodes)
-size[path_nodes] <- 2
-
-p <- ggtree(tree, aes(color = group_colors, linetype = group, size = size)) +
-    scale_size(range = c(0.5, 1.5)) +
-    scale_color_manual(values = c("green", "black", "gainsboro")) +
-    theme(legend.position = "none")
-p
-
-ggsave(
-    filename = file.path(output_plot_dir, "parallelity.svg"),
-    plot = p,
-    device = "svg",
-    width = 3.5,
-    height = 3.5 * tree_height_ratio
-)
